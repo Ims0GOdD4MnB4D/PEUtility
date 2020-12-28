ee886f3fae9a39425c45c579a2bf4a2909e87ca7
Ims0GOdD4MnB4D <unichtojenmusor@gmail.com>
2020-12-29 00:31:40 +0300

    merging labs

--- model/headers/headers/DataDirectoryHeader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-typedef struct MY_IMAGE_DATA_DIRECTORY {
-    const unsigned long VirtualAddress;
-    const unsigned long Size;
-} MY_IMAGE_DATA_DIRECTORY, *MY_PIMAGE_DATA_DIRECTORY;
-
--- model/headers/tables/ImportTable.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef struct {
-    unsigned long *OriginalFirstThunk;
-    unsigned long TimeDateStamp;
-    unsigned long ForwarderChain;
-    unsigned long Name;
-    unsigned long *FirstThunk;
-} IMAGE_IMPORT_DESCRIPTOR;
